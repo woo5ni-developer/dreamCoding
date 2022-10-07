a1d1076d656846ad69199c7d6dea33f30d2d8e38
woowoni <ewoowon.2@gmail.com>
2022-10-07 19:09:11 +0900

    풀더 구조 정리

--- 2.operator/6.relational.js
+++
@@ -1,9 +0,0 @@
-//대소 관계 비교 연산자(Relational operators)
-// > (크다)
-// < (작다)
-// >= (크거나 같다)
-// <= (작거나 같다)
-
-console.log(2 > 3);
-console.log(2 < 3);
-console.log(2 <= 3);
--- 2.operator/7.priority.js
+++
@@ -1,8 +0,0 @@
-let a = 2;
-let b = 3;
-let result = (a + b) * 4;
-console.log(result);
-
-result = a++ + b * 4;
-console.log(result); //이문이 끝난 다음에 a값이 +1
-console.log(a);
